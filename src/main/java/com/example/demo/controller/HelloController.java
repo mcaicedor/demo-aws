b86326610f2e0d8bd06716c1c85bca46abeac6af
Diego Armando Gomez <diegomezusb@gmail.com>
2021-02-19 20:12:46 -0500

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+	
+	@GetMapping
+	public String index() {
+		return "Hello from Spring Boot and AWS";
+	}
+	
+	@GetMapping("/{name}")
+	public String index(@PathVariable("name") String name) {
+		return "Hello "+name+" from Spring Boot and AWS";
+	}
+
+}
